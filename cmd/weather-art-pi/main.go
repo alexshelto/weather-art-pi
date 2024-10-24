c63358d780887a07778c65b5cbebcda4972d2c4d
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2024-10-24 13:04:18 -0400

    add env vars and parsing

---
+++ cmd/weather-art-pi/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log"
+
+    "github.com/alexshelto/weather-art-pi/internal/config"
+)
+
+func main() {
+	// Command-line flag to specify mock temperature
+	mockTemp := flag.Float64("temp", -999, "Mock temperature (°F) to use for testing (bypasses API call)")
+	flag.Parse()
+
+	// Load config from .env file
+	weatherApiConfig, err := config.LoadWeatherApiConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	// Check if mock temperature is provided
+	if *mockTemp != -999 {
+		fmt.Printf("Using mock temperature: %.2f°F\n", *mockTemp)
+	} else {
+        fmt.Println("Not using mock temperature")
+    }
+
+    fmt.Println(weatherApiConfig)
+}
+
+
+
+
---
+++ internal/config/weatherApiConfig.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+    "fmt"
+    "os"
+
+    "github.com/joho/godotenv"
+)
+
+type WeatherApiConfig struct {
+    ApiKey string
+    Lat    string
+    Long   string
+}
+
+func LoadWeatherApiConfig() (*WeatherApiConfig, error) {
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        return nil, fmt.Errorf("error loading .env file: %v", err)
+    }
+
+    weatherApi := &WeatherApiConfig {
+        ApiKey: os.Getenv("API_KEY"),
+        Lat:    os.Getenv("LATITUDE"),
+        Long:   os.Getenv("LONGITUDE"),
+    }
+
+    if weatherApi.ApiKey == "" || weatherApi.Lat == "" || weatherApi.Long == "" {
+        return nil, fmt.Errorf("missing required environment variables")
+    }
+
+    return weatherApi, nil
+}
+
